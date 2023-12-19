3a9343bda2aa91e4ec7305b9363d75f3b02115d1
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2023-12-19 16:37:45 +0330

    end UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./App.css";
+import Testimonials from "./assets/Testimonials/Testimonials";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Plans from "./components/Plans/Plans";
 import Program from "./components/Program/Program";
 import Reasons from "./components/Reasons/Reasons";
+import Join from "./components/join/Join";
 function App() {
   return (
     <div className="App">
@@ -10,6 +13,9 @@ function App() {
       <Program />
       <Reasons />
       <Plans />
+      <Testimonials />
+      <Join />
+      <Footer />
     </div>
   );
 }
