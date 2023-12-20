f5ba9b4abab82ae09a9f7fdf98716bcc0c5bcb00
Mohammad Amin Rajabi <mohammadaminrajabii2004@gmail.com>
2023-12-20 12:01:02 +0330

    end

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,14 +5,24 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
+import NumberCounter from "number-counter";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerWidth <= 768 ? true : false;
+
   return (
-    <div className="hero">
+    <div className="hero" id="home">
+      <div className="blur hero-blur"></div>
       <div className="left-h">
         <Header />
         <div className="the-best-ad">
-          <div> </div>
+          <motion.div
+            initial={{ left: mobile ? "165px" : "238px" }}
+            whileInView={{ left: "8px" }}
+            transition={{ ...transition, type: "tween" }}
+          ></motion.div>
           <span>the best fitness club in the town</span>
         </div>
 
@@ -33,15 +43,21 @@ const Hero = () => {
         </div>
         <div className="figures">
           <div>
-            <span>+140</span>
+            <span>
+              <NumberCounter end={140} start={90} delay="4" preFix="+" />
+            </span>
             <span>expert coachs</span>
           </div>
           <div>
-            <span>+978</span>
+            <span>
+              <NumberCounter end={978} start={750} delay="4" preFix="+" />
+            </span>
             <span>members joine</span>
           </div>
           <div>
-            <span>+50</span>
+            <span>
+              <NumberCounter end={50} start={25} delay="4" preFix="+" />
+            </span>
             <span>fitness programs</span>
           </div>
         </div>
@@ -54,22 +70,40 @@ const Hero = () => {
       <div className="right-h">
         <button className="btn">Join Now</button>
 
-        <div className="heart-rate">
+        <motion.div
+          className="heart-rate"
+          initial={{ right: "-1rem" }}
+          transition={transition}
+          whileInView={{ right: "4rem" }}
+        >
           <img src={heart} alt="" />
           <span>Heart Rate</span>
           <span>116 bpm</span>
-        </div>
+        </motion.div>
 
         <img src={hero_image} alt="" className="hero_image" />
-        <img src={hero_image_back} alt="" className="hero_image_back" />
-        <div className="calories">
+        <motion.img
+          initial={{ right: "11rem" }}
+          whileInView={{ right: "20rem " }}
+          transition={transition}
+          src={hero_image_back}
+          alt=""
+          className="hero_image_back"
+        />
+
+        <motion.div
+          className="calories"
+          initial={{ right: "37rem" }}
+          whileInView={{ right: "28rem" }}
+          transition={transition}
+        >
           <img src={Calories} alt="" />
           <div>
             <span>Calories Burned </span>
             <br />
             <span>220 kcal</span>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
